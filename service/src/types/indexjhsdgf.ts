3d6e0b761f088ca318f273c5a2bcf6680e2291bd
24LP9506 24LP9506 <batbayarsaruuul0@gmail.com>
2025-06-23 19:21:35 +0800

    sadf

--- service/src/types/indexjhsdgf.ts
+++ service/src/types/indexjhsdgf.ts
@@ -16,9 +16,9 @@ dotenv.config();
 const index = pinecone.index("ai-assistant");
 
 const uploadFile = async () => {
-  const embeddings = new OpenAIEmbeddings({
-    openAIApiKey: process.env.NEXT_PUBLIC_BASE_URL ?? "",
-  });
+  // const embeddings = new OpenAIEmbeddings({
+  //   openAIApiKey: process.env.NEXT_PUBLIC_BASE_URL ?? "",
+  // });
 
   const record = {
     id: "rec1",
