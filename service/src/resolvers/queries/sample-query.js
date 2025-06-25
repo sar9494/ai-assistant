41a626aa4a2008995fc4839dfb0c60079f339543
24LP9506 24LP9506 <batbayarsaruuul0@gmail.com>
2025-06-25 14:45:06 +0800

    asdf

---
+++ service/src/resolvers/queries/sample-query.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sampleQuery = void 0;
+const sampleQuery = () => 'Hello sample query';
+exports.sampleQuery = sampleQuery;
