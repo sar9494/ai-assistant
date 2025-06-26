6993b610682aee5f0fadccebf1acc15bbf830ae3
24LP9506 24LP9506 <batbayarsaruuul0@gmail.com>
2025-06-26 13:41:51 +0800

    asdf

--- service/src/prismaClient.js
+++ service/src/prismaClient.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("./generated/prisma/client"); // ✅ output замтай таарна
+const client_1 = require("@/prisma/client"); // ✅ output замтай таарна
 const prisma = new client_1.PrismaClient();
 exports.default = prisma;
