b7a9c53644fb4c1e8632e0c0be9ef71bb228b775
Saruul Batbayar <batbayarsaruul0@gmail.com>
2025-06-29 20:49:52 +0800

    asdf

---
+++ teet/pages/api/graphql/route.ts
@@ -0,0 +1,20 @@
+// pages/api/graphql.ts
+import { createSchema, createYoga } from "graphql-yoga";
+import { resolvers } from "../../../lib/resolvers";
+import { typeDefs } from "../../../lib/schemas";
+
+
+const schema = createSchema({
+  typeDefs,
+  resolvers,
+});
+
+const { handleRequest } = createYoga({
+  schema,
+  graphqlEndpoint: "/api/graphql", // optional
+});
+
+export { handleRequest as GET, handleRequest as POST };
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
--- teet/tailwind.config.js
+++ teet/tailwind.config.js
@@ -1,13 +1,15 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',       // App Router ашиглавал
+    './pages/**/*.{js,ts,jsx,tsx}',     // хуучин Pages Router
+    './components/**/*.{js,ts,jsx,tsx}' // бүх components
   ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')
+  ],
 };
